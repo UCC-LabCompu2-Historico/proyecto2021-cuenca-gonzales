f2c95be2425b5800efe15d378be4c0eff3609cdc
FranciscoGonzalez8668 <francisco_gonzalez2000@hotmail.com>
2021-06-13 16:39:38 -0300

    Se cambian los botones, y se agregan sus funcines, se agrega el boton tratamiento en calendario y se calcula el precio

--- Funciones.js
+++ Funciones.js
@@ -1,3 +1,4 @@
+
 function DibujarFact()
 {
     var canvas = document.getElementsById("canvasdibujar");
@@ -10,5 +11,74 @@ function DibujarFact()
     ctx.strokeStyle="green";
     ctx.lineWidth = 2;
     ctx.strokeText("strokeText",10,120);
+
+}
+/**
+ * Cambiar de pagina
+ * @method cambiarPagina
+ * @param {string} id- el id de la pagina que quiere abrir
+ * @return Valor que retorna
+ */
+ function cambiarPagina(id){
+    switch (id){
+        case "Home":
+            window.open("Index.HTML");
+            break;
+        case "Evo":
+            window.open("Evo.HTML");
+            break;
+        case "Tratamiento":
+            window.open("Tratamiento.HTML");
+            break;
+        case "RecupCapilar":
+            window.open("RecupCap.HTML");
+            break;
+        case "Tecnol":
+            window.open("Tecnol.HTML");
+            break;
+        case "Turnos":
+            window.open("Calendario.HTML");
+            break;
+        case "Instagram":
+            window.open("https://www.instagram.com/mateocuenca09/?hl=es-la");
+            break;
+    }
+
+
+}
+/**
+ * Mostrar en pantalla el precio segun la opcion elegida
+ * @method calcularPrecio
+ * @param {string} id- el id de la opcion elegida
+ * @return Valor que retorna
+ */
+function calcularPrecio(host){
+    /*<option>Tratamiento Simple</option>
+        <option>Tratamiento Medio</option>
+        <option>Tratamiento Grande</option>
+        <option>Recup Capilar</option>
+        <option>Tecnol 1</option>
+        <option>Tecnol 2</option>*/ 
+    switch (host){
+        case "Tratamiento_Simple":
+            document.getElementsByName('precio')[0].innerHTML= "$300";
+            break;
+        case "Tratamiento_Medio":
+            document.getElementsByName('precio')[0].innerHTML= "$450";
+            break;
+        case "Tratamiento_Grande":
+            document.getElementsByName('precio')[0].innerHTML= "$500";
+            break;
+        case "Recup_Capilar":
+            document.getElementsByName('precio')[0].innerHTML= "$420";
+            break;
+        case "Tecnol_1":
+            document.getElementsByName('precio')[0].innerHTML= "$600";
+            break;
+        case "Tecnol_2":
+            document.getElementsByName('precio')[0].innerHTML= "$650";
+            break;
+
+    }
 
 }
