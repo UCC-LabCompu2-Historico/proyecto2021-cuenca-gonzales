85df6fb3439f440164ed2e83d7d508434eab0a6b
MateoCuenca09 <cuencamatu@gmail.com>
2021-06-15 15:49:00 -0300

    Se añaden datos personales al canvas factura

--- Funciones.js
+++ Funciones.js
@@ -10,14 +10,29 @@ function DibujarFact() {
     let x = document.getElementById('input_Tratamiento').value;
     let precio = calcularPrecio(x);
     let hora = document.getElementById('input_horario').value;
+    let fecha = document.getElementById("input_dia").value;
 
     total += precio;
     ser.fillStyle = "#ffffff";
     ser.fillRect(555, 390, 100, 18);
 
-    ctx.font = "13pt Sin Serif";
+    ser.fillStyle = "#ffffff";
+    ser.fillRect(10, 413, 648, 45);
+
+    ctx.font = "12pt Sin Serif";
     ctx.fillStyle = "black";
 
+    let nombre = document.getElementById("input_nombre").value;
+    let apellido = document.getElementById("input_apellido").value;
+    let mail = document.getElementById("input_email").value;
+    let cel = document.getElementById("input_cell").value;
+    ctx.fillText(nombre, 12, 430);
+    ctx.fillText(apellido, 12, 450);
+    ctx.fillText(cel, 220, 430);
+    ctx.fillText(mail, 220, 450);
+
+    ctx.font = "13pt Sin Serif";
+    ctx.fillStyle = "black";
 
     switch (cont) {
         case 1:
@@ -25,17 +40,20 @@ function DibujarFact() {
             ctx.fillText(x, 12, 100);
             ctx.fillText(precio, 555, 100);
             ctx.fillText(hora, 342, 100);
+            ctx.fillText(fecha, 400, 100);
 
             break;
         case 2:
             ctx.fillText(x, 12, 120);
             ctx.fillText(precio, 555, 120);
             ctx.fillText(hora, 342, 120);
+            ctx.fillText(fecha, 400, 120);
             break;
         case 3:
             ctx.fillText(x, 12, 140);
             ctx.fillText(precio, 555, 140);
             ctx.fillText(hora, 342, 140);
+            ctx.fillText(fecha, 400, 140);
             break;
         default:
             alert("No hay mas Tratamientos");
@@ -47,12 +65,11 @@ function DibujarFact() {
         canvas.width = canvas.width;
         cargarCan();
         alert("Faltan datos!");
-        cont = document.getElementsByName("peTurno")[0].value = cont - 1; z
+        cont = document.getElementsByName("peTurno")[0].value = cont - 1;
+        z
     }
 
     ctx.fillText(total, 555, 403);
-
-
 
 
     //const yMax = canvas.height;
